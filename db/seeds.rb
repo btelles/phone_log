87abbf227dc87de1eaf1d2113a6ccee52d154321
Bernie Telles <bernardo.telles@dms.myflorida.com>
2011-08-13 16:42:50 -0400

    add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-#
+
+def set_old_created_at_for_cucumber(tables)
+  tables.each {|table| table.to_s.classify.constantize.update_all("#{table}.created_at = date('1950-01-01 01:01:01')")}
+end
+
+require 'active_record/fixtures'
+user = User.create(:email => 'g_admin') #, :role => role)
+set_old_created_at_for_cucumber(['users'])
+
+# All environments
+all_seeds = Dir["db/seeds/*.csv"].map { |file| File.basename(file, '.csv').to_sym }
+Fixtures.create_fixtures('db/seeds', all_seeds)
+set_old_created_at_for_cucumber(all_seeds)
+
+#The current environment
+environment_specific_seeds = Dir["db/seeds/#{Rails.env}/*.csv"].map { |file| File.basename(file, '.csv').to_sym }
+Fixtures.create_fixtures("db/seeds/#{Rails.env}", environment_specific_seeds)
+set_old_created_at_for_cucumber(environment_specific_seeds)
+
+
+
 User.create(email: 'btelles@gmail.com',
             password: 'aaaaaa',
             password_confirmation: 'aaaaaa')
